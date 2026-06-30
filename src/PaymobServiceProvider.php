6a26db84b2a78ec57537029566f9bd8009bd9eab
Yasser <yassergharieb14@gmail.com>
2026-07-01 02:44:27 +0300

    Revert "Reapply "feat: introduce typed paymob client flow contract and DTOs""
    This reverts commit d37b35223a19616e9f40180f34ff57fb6f76cd5b.

--- src/PaymobServiceProvider.php
+++ src/PaymobServiceProvider.php
@@ -1,31 +1,27 @@
 <?php
 
 namespace Paymob\Laravel;
-
-use Paymob\Laravel\Contracts\PaymobClientContract;
 use Illuminate\Support\ServiceProvider;
-
-class PaymobServiceProvider extends ServiceProvider
+class PaymobServiceProvider extends  ServiceProvider
 {
-    public function boot()
-    {
+
+    public function boot() {
         $this->publishes([
-            __DIR__ . '/../config/paymob.php' => config_path('paymob.php'),
-        ], 'paymob-config');
+            __DIR__.'/../config/paymob.php' => config_path('paymob.php'),
+        ] , 'paymob-config');
+
+
     }
 
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/paymob.php', 'paymob');
+    public function register() {
+
+        $this->mergeConfigFrom(__DIR__.'/../config/paymob.php', 'paymob');
 
         $this->app->singleton(PaymobClient::class, function ($app) {
             return new PaymobClient($app['config']->get('paymob'));
         });
 
-        $this->app->singleton(PaymobClientContract::class, function ($app) {
-            return $app->make(PaymobClient::class);
-        });
-
-        $this->app->alias(PaymobClientContract::class, 'paymob');
+        $this->app->alias(PaymobClient::class, 'paymob');
     }
+
 }
